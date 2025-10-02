c14ab2ed5323825403684e85e75b252a26a58dee
Margarita <margaritagarrido95@hotmail.com>
2025-10-02 21:10:00 +0200

    apartado3

--- Modulo8/src/main.ts
+++ Modulo8/src/main.ts
@@ -78,7 +78,6 @@ const pacientes: Paciente[] = [
 // Pacientes asignados a pediatria:
 
 const obtenPacientesAsignadosAPediatria = (pacientes: Paciente[]): Paciente[] => {
-  // Tu implementación aquí :)
   let pacientesDePediatria : Paciente [] = [];
   let i = 0;
   while (i < pacientes.length) {
@@ -91,35 +90,52 @@ const obtenPacientesAsignadosAPediatria = (pacientes: Paciente[]): Paciente[] =>
 };
 
 console.log(obtenPacientesAsignadosAPediatria(pacientes));
-/*//Pacientes asignados a pediatria <10 años:
 
-const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
-  pacientes: Pacientes[]
-): Pacientes[] => {
-  // Tu implementación aquí :)
+
+//Pacientes asignados a pediatria <10 años:
+
+const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (pacientes: Paciente[]): Paciente[] => {
+  let pacientesPediatraMenoresDeDiez : Paciente [] = [];
+  let i = 0;
+  while ( i < pacientes.length) {
+  if(pacientes[i].especialidad === "Pediatra" && pacientes[i].edad <= 10) {
+    pacientesPediatraMenoresDeDiez = [...pacientesPediatraMenoresDeDiez, pacientes[i]];
+  }
+  i++;
+  }
+  return pacientesPediatraMenoresDeDiez;
 };
+
+console.log(obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes));
 
 //APARTADO2:
 
 /*Queremos activar el protocolo de urgencia si cualquier de los pacientes tiene un ritmo cardíaco
  superior a 100 pulsaciones por minuto y una temperatura corporal superior a 39 grados:*/
 
- const activarProtocoloUrgencia = (pacientes: Pacientes[]): boolean => {
+ const activarProtocoloUrgencia = (pacientes: Paciente[]): boolean => {
   let activarProctolo = false;
-
-  // Tu implementación aquí :)
-
-  return activarProctolo;
+  let i = 0;
+  while(i < pacientes.length) {
+  if(pacientes[i].frecuenciaCardiaca >= 100 && pacientes[i].temperatura >= 39) {
+    activarProctolo = true;
+    break;
+}
+i++;
+  }
+    return activarProctolo;
 };
+
+console.log("Activar protocolo de urgencia", activarProtocoloUrgencia(pacientes));
 
 //APARTADO3:
 
 //Reasignar los pacientes asignados a la especialidad de pediatría a la de medico de familia:
 
 
-const reasignaPacientesAMedicoFamilia = (
-  pacientes: Pacientes[]
-): Pacientes[] => {
+/*const reasignaPacientesAMedicoFamilia = (
+  pacientes: Paciente[]
+): Paciente[] => {
   // Tu implementación aquí :)
 };
 
@@ -128,7 +144,7 @@ const reasignaPacientesAMedicoFamilia = (
 /*Queremos saber si podemos mandar al Pediatra a casa (si no tiene pacientes asignados),
  comprobar si en la lista hay algún paciente asignado a pediatría*/
 
- const HayPacientesDePediatria = (pacientes: Pacientes[]): boolean => {
+ /*const HayPacientesDePediatria = (pacientes: Paciente[]): boolean => {
   // Tu implementación aquí :)
 };
 
@@ -137,14 +153,14 @@ const reasignaPacientesAMedicoFamilia = (
 /*Queremos calcular el número total de pacientes que están asignados a la especialidad de Medico de familia, 
 y lo que están asignados a Pediatría y a cardiología*/
 
-interface NumeroPacientesPorEspecialidad {
+/*interface NumeroPacientesPorEspecialidad {
   medicoDeFamilia: number;
   pediatria: number;
   cardiologia: number;
 }
 
 const cuentaPacientesPorEspecialidad = (
-  pacientes: Pacientes[]
+  pacientes: Paciente[]
 ): NumeroPacientesPorEspecialidad => {
   // Tu implementación aquí :)
-};
+};*/
