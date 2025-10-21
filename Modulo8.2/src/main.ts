24e5ac517d6db662209735a501e071b917c24f31
Margarita <margaritagarrido95@hotmail.com>
2025-10-21 18:56:18 +0200

    Corrección

--- Modulo8.2/src/main.ts
+++ Modulo8.2/src/main.ts
@@ -95,7 +95,7 @@ console.log(obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios);
 /*Queremos activar el protocolo de urgencia si cualquier de los pacientes tiene un ritmo cardíaco superior a 100 pulsaciones
  por minuto y una temperatura corporal superior a 39 grados*/
 
-const activarProtocoloUrgencia = pacientes.filter((pacientes): boolean => {
+const activarProtocoloUrgencia = pacientes.some((pacientes): boolean => {
  let activarProctolo = false;
   if (pacientes.frecuenciaCardiaca >= 100 && pacientes.temperatura >= 39) {
   activarProctolo = true;
