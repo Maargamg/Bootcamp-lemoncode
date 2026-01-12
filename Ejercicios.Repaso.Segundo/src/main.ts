b9a38474233dcb4020c389f59a868d10ee2be800
Margarita <margaritagarrido95@hotmail.com>
2026-01-12 19:13:09 +0100

    más ejercicios repaso

---
+++ Ejercicios.Repaso.Segundo/src/main.ts
@@ -0,0 +1,71 @@
+//Más ejercicios básicos
+//Resta, multiplicación y división: Pide dos números y muestra: La resta, La multiplicación, La división.
+//Número par o impar: Pide un número entero e indica si es par o impar.
+//Mayor de tres números: Pide tres números y muestra cuál es el mayor.
+/*Calcular el área de un rectángulo:Pide la base y la altura de un rectángulo y muestra su área.
+Fórmula: Área = base × altura*/
+/*Conversión de grados Celsius a Fahrenheit
+Pide una temperatura en grados Celsius y conviértela a Fahrenheit.
+Fórmula: F = (C × 9/5) + 32*/
+/*Calculadora simple: Pide dos números y una operación (+, -, *, /) y muestra el resultado según la operación
+elegida.*/
+//Calcular el salario: Pide el número de horas trabajadas y el precio por hora. Calcula y muestra el salario total
+/*Descuento en una compra: Pide el precio de un producto.
+Si el precio es mayor a 100, aplica un 10% de descuento
+Si no, muestra el precio sin descuento*/
+/*/Cálculo de una factura: Pide la cantidad total de una compra y muestra el desglose de una factura:
+Subtotal.
+Precio iva (por ejemplo el 21%).
+Total (subtotal + iva).*/
+//Como el anterior: Ten en cuenta que si es mayor a 100 aplica un 10% de descuento. Desglosa el detalle de la factura.
+/*Intercambiar dos valores:
+Pide dos variables A y B y muestra sus valores intercambiados.
+Ejemplo:
+Antes: A = 5, B = 10 Después: A = 10, B = 5*/
+//Calcular el promedio de dos notas: Pide dos notas y muestra el promedio. Indica si el promedio es aprobado (≥ 5) o suspendido.
+//Número dentro de un rango: Pide un número e indica si está entre 1 y 100 (incluidos).
+//Comparar dos edades: Pide la edad de dos personas e indica quién es mayor o si tienen la misma edad.
+/*Convertir minutos a horas y minutos:
+Pide una cantidad de minutos y conviértela a horas y minutos.
+Ejemplo: 130 minutos → 2 horas y 10 minutos*/
+/*Clasificación de un número
+Pide un número e indica si es:
+Menor que 10
+Entre 10 y 100
+Mayor que 100*/
+/*Calcular el perímetro de un cuadrado
+Pide el lado de un cuadrado y muestra su perímetro.
+Fórmula: Perímetro = 4 × lado*/
+/*18 Validar contraseña simple:
+Pide una contraseña y verifica si es igual a "1234". Muestra Acceso permitido o Acceso
+denegado.*/
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+//Ejercicios para modificar el DOM
+//Cambiar texto de un párrafo: Crea un párrafo con un id. Al pulsar un botón, cambia su texto por otro diferente.
+//Cambiar el color de un texto: Al hacer clic en un botón, cambia el color de un texto a rojo.
+//Mostrar un mensaje oculto: Crea un texto oculto (display: none). Al pulsar un botón, haz que el texto se muestre.
+//Ocultar un elemento: Al pulsar un botón, oculta un párrafo de la página.
+//Cambiar el tamaño de la letra: Al hacer clic en un botón, aumenta el tamaño de la letra de un texto
+//Cambiar el texto usando un input: Crea un input y un botón. Al pulsar el botón, el texto del input debe aparecer en un párrafo.
+//Cambiar una imagen: Muestra una imagen. Al pulsar un botón, cambia la imagen por otra diferente.
+//Cambiar el fondo de la página: Al pulsar un botón, cambia el color de fondo del body.
+//Activar y desactivar una clase: Crea un botón que añada o quite una clase CSS a un elemento.
+//Contador simple (sin bucles):Crea un número inicial (por ejemplo 0). Cada vez que se pulse un botón, el número debe aumentar en 1 y mostrarse en pantalla.
+//Cambiar texto al pasar el ratón: Cuando el usuario pase el ratón sobre un texto (mouseover), cambia su contenido. Cuandosalga (mouseout), vuelve al texto original.
+//Validar un input vacío: Crea un input y un botón. Si el usuario pulsa el botón sin escribir nada, muestra un mensajede error.
+/*Cambiar texto según una condición:
+Crea un botón que:
+Muestre “Encendido” si el texto actual es “Apagado”
+Muestre “Apagado” si el texto actual es “Encendido”*/
+//Cambiar el contenido de un div: Crea un div vacío. Al pulsar un botón, añade texto dentro del div.
+//Mostrar la hora actual: Al pulsar un botón, muestra la hora actual dentro de un párrafo usando Date.
+//Cambiar el texto de varios elementos (sin bucles): Crea dos o tres elementos con distintos id. Al pulsar un botón, cambia el texto de cada uno manualmente.
+//Cambiar atributo disabled: Crea un botón deshabilitado. Al pulsar otro botón, habilítalo.
+//Cambiar el placeholder de un input: Al pulsar un botón, cambia el texto del placeholder de un input.
+//Cambiar el texto del propio botón Al hacer clic en un botón, cambia su propio texto.
+//20 Confirmación visual: Al pulsar un botón, muestra un mensaje y cambia su color a verde indicando que la acción fue correcta.
