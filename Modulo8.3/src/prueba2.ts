8db7af10787e7cda207d2eb2bb78ad8c04a6788e
Margarita <margaritagarrido95@hotmail.com>
2026-02-06 20:23:29 +0100

    Prueba de conscepto 2

---
+++ Modulo8.3/src/prueba2.ts
@@ -0,0 +1,16 @@
+const girarFoto = document.getElementById("fotos");
+if(girarFoto !== null && girarFoto !== undefined && girarFoto instanceof HTMLDivElement) {
+girarFoto.addEventListener("click", () => {
+   const fotoAnimal = document.getElementById("foto1");
+   if(fotoAnimal !== null && fotoAnimal !== undefined && fotoAnimal instanceof HTMLImageElement) {
+    fotoAnimal.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/1.png";
+   }
+})
+};
+
+
+
+
+
+
+
