a0512d89d35712201926462750331c2b30e179e9
Margarita <margaritagarrido95@hotmail.com>
2026-02-12 20:26:08 +0100

    ejercicio

--- Modulo8.3/src/prueba5.ts
+++ Modulo8.3/src/prueba5.ts
@@ -1,1 +1,9 @@
 const arrayCartas : string[] = ["leon", "buho", "perro", "gallina", "cerdo", "abeja", "leon", "buho", "perro", "gallina", "cerdo", "abeja"];
+
+const escucharClick = document.querySelectorAll(".carta");
+escucharClick.forEach((carta) =>{
+if(carta !== null && carta !== undefined && carta instanceof HTMLDivElement) {
+    const indice = carta.getAttribute("data-indice-id");
+    console.log(indice);
+}
+} )
