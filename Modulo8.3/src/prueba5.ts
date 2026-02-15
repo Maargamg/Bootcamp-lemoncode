4a1072f130d2fb688c36dd29cee2e2b3859368d4
Margarita <margaritagarrido95@hotmail.com>
2026-02-15 20:09:30 +0100

    Prueba de concepto 5

--- Modulo8.3/src/prueba5.ts
+++ Modulo8.3/src/prueba5.ts
@@ -1,9 +1,35 @@
-const arrayCartas : string[] = ["leon", "buho", "perro", "gallina", "cerdo", "abeja", "leon", "buho", "perro", "gallina", "cerdo", "abeja"];
-
-const escucharClick = document.querySelectorAll(".carta");
-escucharClick.forEach((carta) =>{
-if(carta !== null && carta !== undefined && carta instanceof HTMLDivElement) {
-    const indice = carta.getAttribute("data-indice-id");
-    console.log(indice);
+interface infoCarta {
+  idFoto: number;
+  imagen: string;
 }
-} )
+
+const cartasAnimales: infoCarta[] = [
+{idFoto: 1,imagen: "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/1.png"},
+{idFoto: 2,imagen: "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/2.png"},
+{idFoto: 3,imagen: "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/3.png"},
+{idFoto: 4,imagen: "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/4.png"},
+{idFoto: 5,imagen: "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/5.png"},
+{idFoto: 6,imagen: "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/6.png"}
+];
+
+const arrayAnimales: infoCarta[] = [...cartasAnimales, ...cartasAnimales];
+
+const posicionArray = document.querySelectorAll(".carta");
+ for (let i = 0; i < posicionArray.length; i++) {
+const posicion = posicionArray[i] as HTMLDivElement;
+posicion.addEventListener("click", () => {
+    const index = posicion.getAttribute("data-indice-id");
+if(!index) return;
+
+const posicionIndice = Number(index);
+const carta : infoCarta = arrayAnimales[posicionIndice];
+
+const img = posicion.querySelector("img");
+if(img !== null && img !== undefined && img instanceof HTMLImageElement) {
+ img.src = carta.imagen;
+}
+
+console.log(`La posicion en el array que has pulsado es ${index}, idFoto ${carta.idFoto}`);
+
+})
+};
