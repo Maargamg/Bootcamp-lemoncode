a7328e53e973b4fdca5f504c335d984398de14ae
Margarita <margaritagarrido95@hotmail.com>
2026-02-21 13:25:17 +0100

    mensaje victoria

--- Modulo8.3/src/ui.ts
+++ Modulo8.3/src/ui.ts
@@ -5,17 +5,35 @@ import {
   voltearLaCarta, 
   sonPareja, 
   parejaEncontrada, 
-  parejaNoEncontrada 
+  parejaNoEncontrada, 
+  esPartidaCompleta
 } from "./motor";
+
+
+
+const h1 = document.querySelector("h1") as HTMLHeadingElement;
+const h2 = document.querySelector("h2") as HTMLHeadingElement;
+const intentos = document.getElementById("intentos") as HTMLInputElement;
+const cartas = document.getElementById("cartas") as HTMLDivElement;
 
 const botonPlay = document.getElementById("start");
 if(botonPlay !== null && botonPlay !== undefined && botonPlay instanceof HTMLButtonElement) {
   botonPlay.addEventListener("click", () => {
     iniciaPartida(tablero);
+    h1.style.display = "block";
     botonPlay.disabled = true;
+    h2.style.display = "none";
+    intentos.style.display = "flex";
+    cartas.style.display = "grid";
   });
 }
 
+const contadorIntentos = document.getElementById("intentos");
+if(contadorIntentos !== null && contadorIntentos !== undefined && contadorIntentos instanceof HTMLInputElement){
+contadorIntentos.value = "00";
+}
+
+let contador = 0;
 let primeraCartaVolteada: number | null = null;
 let segundaCartaVolteada: number | null = null;
 let bloqueado = false; 
@@ -25,63 +43,81 @@ posicionArray.forEach((posicion, i) => {
   const divCarta = posicion as HTMLDivElement;
 
   divCarta.addEventListener("click", () => {
+  divCarta.classList.add("animacion");
+ setTimeout(() => {
+  divCarta.classList.remove("animacion");
+}, 300);
     if (bloqueado) return; 
-    const indice = i;
+  const indice = i;
+ 
 
-  
-    if (!sePuedeVoltearLaCarta(tablero, indice)) return;
+  const mensaje = document.getElementById("mensaje");
+  if(mensaje !== null && mensaje !== undefined && mensaje instanceof HTMLDivElement) {
+  if (!sePuedeVoltearLaCarta(tablero, indice)) {
+  mensaje.textContent = "¡¡Esta carta ya está girada!!";
 
-    voltearLaCarta(tablero, indice);
-    const posicionCarta = tablero.cartas[indice];
-    const img = divCarta.querySelector("img") as HTMLImageElement;
+  setTimeout(() => {
+    mensaje.textContent = "";
+  }, 1500);
+
+  return;
+}
+  voltearLaCarta(tablero, indice);
+  const posicionCarta = tablero.cartas[indice];
+  const img = divCarta.querySelector("img") as HTMLImageElement;
     img.src = posicionCarta.imagen;
     img.style.display = "block";
     divCarta.style.backgroundColor = "transparent";
 
     
-    if (primeraCartaVolteada === null) {
+  if (primeraCartaVolteada === null) {
       primeraCartaVolteada = indice;
       tablero.estadoPartida = "UnaCartaLevantada";
-    } else if (segundaCartaVolteada === null && indice !== primeraCartaVolteada) {
-      segundaCartaVolteada = indice;
-      tablero.estadoPartida = "DosCartasLevantadas";
-      bloqueado = true; 
+    } else if (segundaCartaVolteada === null && indice !== primeraCartaVolteada && contadorIntentos !== null &&
+    contadorIntentos instanceof HTMLInputElement) {
+    segundaCartaVolteada = indice;
+    tablero.estadoPartida = "DosCartasLevantadas";
+    bloqueado = true; 
+    contador++;
+    contadorIntentos.value = contador.toString().padStart(2, "0");
 
     
-      if (sonPareja(primeraCartaVolteada, segundaCartaVolteada, tablero)) {
-        parejaEncontrada(tablero, primeraCartaVolteada, segundaCartaVolteada);
-        bloqueado = false;
-        primeraCartaVolteada = null;
-        segundaCartaVolteada = null;
-        tablero.estadoPartida = "CeroCartasLevantadas";
-      } else {
-        
-        setTimeout(() => {
-          parejaNoEncontrada(tablero, primeraCartaVolteada!, segundaCartaVolteada!);
-
-          
-          [primeraCartaVolteada!, segundaCartaVolteada!].forEach(i => {
-            const cartaDiv = posicionArray[i] as HTMLDivElement;
-            const img = cartaDiv.querySelector("img") as HTMLImageElement;
-            img.style.display = "none";
-            cartaDiv.style.backgroundColor = "transparent"; 
+  if (sonPareja(primeraCartaVolteada, segundaCartaVolteada, tablero)) {
+    parejaEncontrada(tablero, primeraCartaVolteada, segundaCartaVolteada);
+    const fin = document.getElementById("victoria");
+    if(fin !== null && fin !== undefined && fin instanceof HTMLDivElement ){
+    if (esPartidaCompleta(tablero)) {
+   h1.style.display = "none";
+   intentos.style.display = "none";
+   cartasDiv.style.display = "none";
+   botonPlay.style.display = "none";
+   fin.textContent = "🎉 ¡HAS GANADO! 🎉";
+   fin.style.display = "block";
+}}
+    bloqueado = false;
+    primeraCartaVolteada = null;
+    segundaCartaVolteada = null;
+    tablero.estadoPartida = "CeroCartasLevantadas";
+    } else {
+     setTimeout(() => {
+      parejaNoEncontrada(tablero, primeraCartaVolteada!, segundaCartaVolteada!);
+      [primeraCartaVolteada!, segundaCartaVolteada!].forEach(i => {
+        const cartaDiv = posicionArray[i] as HTMLDivElement;
+        const img = cartaDiv.querySelector("img") as HTMLImageElement;
+        img.style.display = "none";
+        cartaDiv.style.backgroundColor = ""; 
           });
 
           
-          primeraCartaVolteada = null;
-          segundaCartaVolteada = null;
-          tablero.estadoPartida = "CeroCartasLevantadas";
-          bloqueado = false; 
+        primeraCartaVolteada = null;
+        segundaCartaVolteada = null;
+        tablero.estadoPartida = "CeroCartasLevantadas";
+        bloqueado = false; 
         }, 1000);
       }
     }
-  });
+ }});
 });
 
 
-/*Apartados opcionales
-Que mejoras puedes implementar:
 
-Mostrar cuantos intentos lleva el usuario.
-Mostrar una animación cuando el usuario pinche en una carta.
-Que si el usuario pincha en una carta ya volteada le salga un mensaje.*/
