35828bada8658474eb28547d27d286ba4d4a8994
Margarita <margaritagarrido95@hotmail.com>
2026-03-22 17:41:58 +0100

    Entrega

---
+++ Modulo9.2/src/ui.ts
@@ -0,0 +1,18 @@
+import {validarClave} from './motor';
+import { commonPasswords } from './motor.helper';
+
+export const mensajeError = document.getElementById("texto") ;
+export const usuario = document.getElementById("usuario");
+export const contraseña = document.getElementById("contraseña");
+if(usuario !== null && usuario !== undefined && usuario instanceof HTMLInputElement &&
+  contraseña !== null && contraseña !== undefined && contraseña instanceof HTMLInputElement &&
+mensajeError !== null && mensajeError !== undefined && mensajeError instanceof HTMLParagraphElement){
+    contraseña.addEventListener("input", () => {
+    const resultado = validarClave(usuario.value, contraseña.value, commonPasswords);
+    if(resultado.esValida) {
+      mensajeError.textContent = "🪄Clave Valida✨";
+    } else {
+      mensajeError.textContent = "❌⛔¡¡Clave no valida!!🚫🔴" + resultado.error;
+    }
+    });
+  }
